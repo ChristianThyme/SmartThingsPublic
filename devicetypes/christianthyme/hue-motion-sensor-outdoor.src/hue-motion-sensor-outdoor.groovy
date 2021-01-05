9b61924560945d712d6aa17db0f780d286a06f1e
Christian Popp Thyme <christianpoppthyme@me.com>
2021-01-05 19:44:09 +0100

    Changes copyright year

--- devicetypes/christianthyme/hue-motion-sensor-outdoor.src/hue-motion-sensor-outdoor.groovy
+++ devicetypes/christianthyme/hue-motion-sensor-outdoor.src/hue-motion-sensor-outdoor.groovy
@@ -1,7 +1,7 @@
 /**
  *  Hue Motion Sensor Outdoor
  *
- *  Copyright 2020 Christian Popp Thyme
+ *  Copyright 2021 Christian Popp Thyme
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
